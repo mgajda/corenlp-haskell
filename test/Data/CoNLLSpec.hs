afb54983465c0cd1c50f7894c923a8c3009e8615
Alejandro D. P <vwwv@correo.ugr.es>
2017-07-26 10:59:34 +0200

    set test suite

---
+++ test/Data/CoNLLSpec.hs
@@ -0,0 +1,24 @@
+
+
+module Data.CoNLLSpec where
+
+
+import           Data.CoNLL
+import           Protolude
+import           Test.Tasty.Hspec
+---------------------------------------------------------------------------
+
+
+
+spec :: Spec
+spec = describe "the conll parser"                $ do
+
+          it "must echo parse every example file" $ do
+            pendingWith "TODO"
+          
+          it "must produce the expected output"   $ do
+            pendingWith "TODO"
+
+
+
+
